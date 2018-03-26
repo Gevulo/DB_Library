30f8ab3ee6c4f8c069a8a8494a390897de66e5b1
Gevulo <oliyarnykoo@gmail.com>
2018-03-26 11:08:00 +0300

    Oleg

---
+++ DB_Library/Facade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Library
+{
+    class Facade
+    {
+        AddBook ab = new AddBook();
+        DeleteBook db = new DeleteBook();
+        LoadBook lb = new LoadBook();
+        SearchBook sb = new SearchBook();
+
+        public void Add()
+        {
+            return;
+        }
+        public void Search()
+        {
+            return;
+        }
+        public void Delete()
+        {
+            return;
+        }
+        public void Load()
+        {
+            return;
+        }
+    }
+}
